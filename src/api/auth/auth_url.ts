aba34ff6f71f5ff80900028896f20a57bf7653d9
TomiPr <tomyprastiyo8@gmail.com>
2024-08-11 13:10:06 +0700

    [update] opn to open version 8.4.2

--- src/api/auth/auth_url.ts
+++ src/api/auth/auth_url.ts
@@ -1,4 +1,4 @@
-import opn from "opn";
+import open from "open";
 import { config } from "../../utils/config";
 
 const baseUrl: string = "https://anilist.co/api/v2/oauth/authorize";
@@ -8,7 +8,7 @@ const responseType: string = "code";
 const getAuthUrl = async (): Promise<string> => {
   const authUrl: string = `${baseUrl}?client_id=${config.anilistClientId}&redirect_uri=${config.anilistRedirect}&response_type=${responseType}`;
 
-  opn(authUrl);
+  await open(authUrl);
   return authUrl;
 };
 
